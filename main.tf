7ce6feb85d9fef40b39caa2d54c27b6c0bdd60d7
lip2050 <lip2050@users.noreply.github.com>
2023-07-26 14:05:03 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "params" {
-  count  = var.params[count.index].name
+  count = length(var.params)
+  name  = var.params[count.index].name
   type  = var.params[count.index].type
   value = var.params[count.index].value
 }
